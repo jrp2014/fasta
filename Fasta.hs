7ba5099d59b8a0c999906522769abb9d84c1d084
John Pavel <jrp@dial.pipex.com>
2018-06-26 20:59:56 +0100

    Step 3

--- Fasta.hs
+++ Fasta.hs
@@ -1,12 +1,11 @@
 module Fasta where
 
-import           Control.Monad
+import Control.Monad
 import Data.List
-import           Data.List.Unique
-import qualified Data.Map         as Map
+import Data.List.Unique
+import qualified Data.Map as Map
 
 -- http://users.csc.calpoly.edu/~dekhtyar/448-Spring2013/lectures/lec08.448.pdf
-
 type Ix = Int -- String index
 
 type DiagIx = Int -- Diagonal index
@@ -23,6 +22,8 @@ type DiagonalScores = Map.Map Int Int
 
 type Subst = Bool -> Score
 
+type Tally = (Int, [Int], Int) -- running tally of matches, previous match lengths, gap count
+
 sigma :: String
 sigma = "ATCG"
 
@@ -31,9 +32,11 @@ s1 = "ATCGTATCG"
 
 t1 = "CAGATCGTCTCGAT"
 
-score :: Bool -> Int
-score True = 5
-score False = -4
+matchScore :: Score
+matchScore = 5
+
+gapScore :: Score
+gapScore = -4
 
 els :: Int -> String -> [String]
 els = replicateM
@@ -43,26 +46,26 @@ ktupleMap k s = Map.fromListWith (++) $ ktuples k s
 
 ktuples :: Int -> String -> [(String, [Ix])]
 ktuples k s
-  | k < lens = ktuples' k 0 lens s
+  | k < lens = ktuples' 0 lens s
   | otherwise = undefined
   where
     lens = length s
-
     -- TODO:: think about whether this is better done with a traverse
-    ktuples' :: Int -> Int -> Int -> String -> IndexList
-    ktuples' k ix len s@(_:t) =
+    ktuples' :: Int -> Int -> String -> IndexList
+    ktuples' ix len s'@(_:t) =
       if ix + k > len
         then []
-        else (take k s, [ix]) : ktuples' k (ix + 1) len t
-    ktuples' _ _ _ _ = undefined
+        else (take k s', [ix]) : ktuples' (ix + 1) len t
+    ktuples' _ _ _ = undefined
 
 diagonal :: Ix -> Ix -> DiagIx
 diagonal i j = i - j
 
 -- TODO:: Check the limits here...
 indexes :: DiagIx -> Int -> Int -> [(Ix, Ix)]
-indexes d m n = [(x, x + d) | x <- [d' .. max m n], x < m + n -1]
-    where d' = max 0 (-d)
+indexes d m n = [(x, x + d) | x <- [d' .. max m n], x < m + n - 1]
+  where
+    d' = max 0 (-d)
 
 diagonals :: Int -> String -> String -> DiagMap
 diagonals k s t = dotplot ls lt
@@ -78,40 +81,63 @@ diagonalScores k s t = bestscores
   where
     diags :: DiagMap
     diags = diagonals k s t
-    
-    fullscores :: [(DiagIx, Score)]
-    fullscores = count $ concat diags
+
+    scores :: [(DiagIx, Score)]
+    scores = count $ concat diags
     -- or count $ foldr (++) [] diags ?
-    
-    scores :: [Int]
-    scores = map snd fullscores
-    
+
     --TODO:: threshold should be within 2 std devs of mean
     discardOutliers :: [(DiagIx, Score)] -> [(DiagIx, Score)]
     discardOutliers = id
-    
-    bestscores :: [(DiagIx, Score)]
-    bestscores = take 10 $ discardOutliers $ sortBy (\(_, x) (_, y) -> compare y x) fullscores
 
+    bestscores :: [(DiagIx, Score)]
+    bestscores =
+      take 10 $ discardOutliers $ sortBy (\(_, x) (_, y) -> compare y x) scores
 
 -- Step 3
-diagonalsRescored :: Int -> String -> String -> Subst -> [(DiagIx, Score)]
---diagonalsRescored k s t subst = map (\(dx, dy) -> subst (dx == dy)) diagIndexes
-diagonalsRescored k s t subst = map diagScore diagCandidates
+-- Eg, diagonalsRescored 3 s1 t1
+
+diagonalsRescored :: Int -> String -> String -> [(DiagIx, Score)]
+diagonalsRescored k s t =
+  map
+    ((\(d, (_, runLengths, gapCount)) ->
+        (d, matchScore * sum (filter (>= k) runLengths) + gapScore * gapCount)) .
+     diagonalRescored)
+    diagCandidates
+--diagonalsRescored k s t = map diagonalRescored diagCandidates
   where
-    lens = length s
-    lent = length t
-    
     diagCandidates :: [DiagIx]
     diagCandidates = map fst $ diagonalScores k s t
-    
-    
---    diagIndex :: DiagIx -> (DiagIx, [(Ix, Ix)])
---    diagIndex d = (d, indexes d lens lent)
---    
---    diagIndexes :: [(DiagIx, [(Ix, Ix)])]
---    diagIndexes = map diagIndex diagCandidates
-   
-    diagScore :: DiagIx -> (DiagIx, Score)
-    diagScore d = (d, sum $ map (\(dx, dy) -> score (dx == dy)) (indexes d lens lent))
-    
+
+    diagonalRescored :: DiagIx -> (DiagIx, Tally)
+    diagonalRescored d
+      | d >= 0 = (d, diagonalRescored' (drop d s) t)
+      | otherwise = (d, diagonalRescored' s (drop (-d) t))
+
+    diagonalRescored' :: String -> String -> Tally
+    diagonalRescored' s' t' = diagonalRescored'' s' t' (0, [], 0)
+    diagonalRescored'' :: String -> String -> Tally -> Tally
+    diagonalRescored'' [] _ (runLength, runLengths, gapCount) =
+      (0, runLength : runLengths, gapCount)
+
+    diagonalRescored'' _ [] (runLength, runLengths, gapCount) =
+      (0, runLength : runLengths, gapCount)
+    diagonalRescored'' (x:xs) (y:ys) (runLength, runLengths, gapCount)
+      | x == y && runLength == k - 1 =
+        match xs ys (runLength + 1, runLengths, gapCount)
+      | x == y = diagonalRescored'' xs ys (runLength + 1, runLengths, gapCount)
+      | otherwise = diagonalRescored'' xs ys (0, runLengths, gapCount)
+      where
+        match :: String -> String -> Tally -> Tally
+        match [] _ (rl, rls, gc) = (0, rl : rls, gc)
+        match _ [] (rl, rls, gc) = (0, rl : rls, gc)
+        match (a:as) (b:bs) (rl, rls, gc)
+          | a == b = match as bs (rl + 1, rls, gc)
+          | otherwise = gap as bs (0, rl : rls, gc + 1)
+
+        gap :: String -> String -> Tally -> Tally
+        gap [] _ tally = tally
+        gap _ [] tally = tally
+        gap (a:as) (b:bs) (rl, rls, gc)
+          | a /= b = gap as bs (rl, rls, gc + 1)
+          | otherwise = match as bs (1, rls, gc)
